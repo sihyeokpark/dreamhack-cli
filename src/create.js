56670cdd1f764cbe1f40b30f775ba55a65b64e52
sihyeokpark <exon.indiv@gmail.com>
2024-01-16 20:15:48 +0900

    🔥 Feature: create docker

--- src/create.js
+++ src/create.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
+import AdmZip from 'adm-zip'
+import { execSync } from 'child_process'
 
 import login from './util/login.js'
 import downloadFile from './util/downloadFile.js'
 
-async function create(wargameLink) {
+async function create(wargameLink, PORT=80) {
   const sessionid = await login()
 
   const wargamePage = await axios.get(wargameLink, {
@@ -12,13 +14,26 @@ async function create(wargameLink) {
     }
   })
 
-  const wargameDownloadLink = 'https://sfo2.digitaloceanspaces.com/' + wargamePage.data.split('문제 파일 받기')[0].split('" target="_blank" class="challenge-download"')[0].split('<a href="https://sfo2.digitaloceanspaces.com/')[1].replaceAll('amp;', '')
+  const wargameName = wargamePage.data.split('class="challenge-info"')[1].split('</h1>')[0].split('>').at(-1).replaceAll(' ', '_')
+  console.log(`[*] Wargame Name - ${wargameName}`)
+  const wargameDownloadLink = 'https://sfo2.digitaloceanspaces.com/' + wargamePage.data.split('" target="_blank" class="challenge-download"')[0].split('<a href="https://sfo2.digitaloceanspaces.com/')[1].replaceAll('amp;', '')
   console.log(`[*] Wargame Download Link - ${wargameDownloadLink}`)
 
-  await downloadFile(wargameDownloadLink, 'wargame.zip')
+  await downloadFile(wargameDownloadLink, `${wargameName}.zip`)
   console.log('[*] Wargame Downloaded')
+
+  const zip = new AdmZip(`${wargameName}.zip`)
+  zip.extractAllTo(`./${wargameName}`, true)
+
+  let dockerCMD = `docker build -t ${wargameName.toLowerCase()} ./${wargameName}`
+  console.log(`[*] Docker Build - ${dockerCMD}`)
+  await execSync(dockerCMD)
+
+  dockerCMD = `docker run -dp ${PORT}:${PORT} ${wargameName.toLowerCase()}` // if web
+  console.log(`[*] Docker Run - ${dockerCMD}`)
+  await execSync(dockerCMD)
 
   console.log()
 }
 
-create(process.argv[2])
+create(process.argv[2], process.argv[3])
